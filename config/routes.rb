1114d523e00f1b8405272a92d559622da8d7e901
mizech <michael.zech@hotmail.de>
2024-10-03 10:21:08 +0200

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'about', to: "page#about"
+
+  root "page#home"
+  # https://www.youtube-nocookie.com/embed/TlgSp2XPCY4?list=PL3mtAHT_eRezB9fnoIcKS4vYFjm23vddb&autoplay=1&iv_load_policy=3&loop=1&start=
+  # Bis 12:41
+end
