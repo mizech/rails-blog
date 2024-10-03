0ddce63c6b315856e3378cf3b829e5e422bc151a
mizech <michael.zech@hotmail.de>
2024-10-03 10:35:58 +0200

    Edited comment

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   root "page#home"
   # https://www.youtube-nocookie.com/embed/TlgSp2XPCY4?list=PL3mtAHT_eRezB9fnoIcKS4vYFjm23vddb&autoplay=1&iv_load_policy=3&loop=1&start=
-  # Bis 12:41
+  # Video bis 12:41
 end
